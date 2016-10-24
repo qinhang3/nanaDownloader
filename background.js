91e11c93328681b3ac7c1ae39e21be13527afc9a
qinhang.qh <qinhang.qh@alibaba-inc.com>
2016-10-24 22:49:43 +0800

    download is running

--- background.js
+++ background.js
@@ -7,28 +7,97 @@
 
 var download = {};
 
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function (info) {
+//         if (download[info.url] == undefined) {
+//             // alert("create " + info.url+ " now url = "+tab.url);
+//             chrome.tabs.create({
+//                 url: info.url,
+//                 active: false,
+//
+//             }, function (tab) {
+//                 download[info.url] = "done";
+//             });
+//         }
+//
+//         return {};
+//     },
+//     // filters
+//     {
+//         urls: [
+//             "https://*.cloudfront.net/*.ts*"
+//         ]
+//         // types: ["other"]
+//     },
+//     // extraInfoSpec
+//     ["blocking"]
+// );
+
+var tabCnt  = 0 ;
+setInterval(function () {
+    chrome.tabs.query({},function(tabs){
+        tabCnt = tabs.length;
+        // console.log(tabCnt);
+    });
+},10);
+
 chrome.webRequest.onBeforeRequest.addListener(
     function (info) {
-        if (download[info.url] == undefined) {
-            // alert("create " + info.url+ " now url = "+tab.url);
-            chrome.tabs.create({
-                url: info.url,
-                active: false,
 
-            }, function (tab) {
-                download[info.url] = "done";
-            });
+        var tokenLength = "904cdf2b-576d-48a1-9529-ad97d3ed1184".length;
+        var length = info.url.length;
+        var token = info.url.substr(length - tokenLength,tokenLength);
+
+        if (download[token] == undefined){
+            download[token] = "done";
+            var pos = info.url.indexOf(".ts");
+            for (var i = 0;i<225;i++){
+                var newUrl = info.url.substring(0,pos-3) + padLeft(i,3) + info.url.substr(pos);
+
+                while(tabCnt > 10){
+                    console.log("waiting "+tabCnt);
+                }
+
+                tabCnt ++;
+
+                chrome.tabs.create({
+                    url: newUrl,
+                    active: false
+
+                }, function (tab) {
+
+                    // download[info.url] = "done";
+                });
+            }
         }
+
+        // if (download[info.url] == undefined) {
+        //     // alert("create " + info.url+ " now url = "+tab.url);
+        //     chrome.tabs.create({
+        //         url: info.url,
+        //         active: false,
+        //
+        //     }, function (tab) {
+        //         download[info.url] = "done";
+        //     });
+        // }
 
         return {};
     },
     // filters
     {
         urls: [
-            "https://*.cloudfront.net/*.ts*"
+            "https://nanairo.co/videos/*.ts*"
         ]
         // types: ["other"]
-    },
+    }
     // extraInfoSpec
-    ["blocking"]
+    // ["blocking"]
 );
+
+function padLeft(str,lenght){
+    if(str.length >= lenght)
+        return str;
+    else
+        return padLeft("0" +str,lenght);
+}
